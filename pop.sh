a437697f1f34b2bfa940fca32ebccb6fa5cf6fbf
John Hoffer <hoffexi@gmail.com>
2016-12-17 18:58:12 -0500

    put all html files in the same directory

--- pop.sh
+++ pop.sh
@@ -1,15 +1,14 @@
 stldir=/home/john/2017/data/3dxp/stl
 popdir=/home/john/2017/data/3dxp/stl/pop/$1
-filename=$1"_smooth1"
+geodir=popdir/popGeo
+filename=$1"_mesh"
 stlname=$filename".stl"
 x3dname=$filename".x3d"
+htmlname=$filename".html"
 
 cd $stldir
-
 mkdir $popdir
-mkdir $popdir/popGeo
+mkdir $geodir
 
-aopt -i $stlname -x $popdir/$x3dname
-
-cd $popdir
-aopt -i $x3dname -K popGeo/:pb -N index.html
+aopt -i $stlname -x $x3dname
+aopt -i $x3dname -K $geodir/:pb -N index.html
