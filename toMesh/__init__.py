4eac38709e6ca023fb57ca6ed1ddac28257c3884
thejohnhoffer <hoffexi@gmail.com>
2016-12-18 15:53:05 -0500

    add new functions smoothvol and savemesh

--- toMesh/__init__.py
+++ toMesh/__init__.py
@@ -1,4 +1,5 @@
 from trace import Trace, tracefill
 from smoothen import Smoothen, smoothmesh
+from smoothen import smoothvol, savemesh
 from thresh import Thresh, threshbound
 from label import Label, countlabels
--- toMesh/smoothen.py
+++ toMesh/smoothen.py
@@ -11,7 +11,7 @@ class Smoothen:
         for y in range(all_y):
             self.smooth_vol[y] = tracefill(self.volume[y])
 
-    def store_mesh(self, filename, bboff):
+    def save_mesh(self, filename, bboff):
 
         arr = [self.smooth_vol,0]
         params = {
@@ -32,8 +32,35 @@ class Smoothen:
             m.save(filename, f)
         return m
 
+def savemesh(volume, filename, bboff):
+
+    print ('Storing mesh at '+where[0])
+    arr = [volume,0]
+    params = {
+        'spacing': (1., 1., 1.,),
+        'gradient_direction':'ascent'
+    }
+    verts, faces = measure.marching_cubes(*arr,**params)
+    applied_verts = verts[faces]
+    vert_count = applied_verts.shape[0]
+
+    mesh_data = np.zeros(vert_count, dtype=mesh.Mesh.dtype)
+
+    for i, v in enumerate(applied_verts):
+        mesh_data[i][1] = v + bboff
+
+    m = mesh.Mesh(mesh_data)
+    with open(filename, 'w') as f:
+        m.save(filename, f)
+    return m
+
+def smoothvol(volume):
+    print('Smoothening volume of '+str(volume.shape))
+    smoothy = Smoothen(volume)
+    return smoothy.smooth_vol
+
 def smoothmesh(volume,*where):
     print('Smoothening volume of '+str(volume.shape))
     smoothy = Smoothen(volume)
     print ('Storing mesh at '+where[0])
-    return smoothy.store_mesh(*where)
+    return smoothy.save_mesh(*where)
