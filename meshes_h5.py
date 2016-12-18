51e45cdc0c0c0de3ac8697b874d62a835caf812e
thejohnhoffer <hoffexi@gmail.com>
2016-12-18 15:18:28 -0500

    fix meshes_h5.py

--- meshes_h5.py
+++ meshes_h5.py
@@ -5,21 +5,19 @@ from toMesh import threshbound
 from toMesh import countlabels
 
 OUT_FOLDER = sys.argv[1]
-OUTNAME = os.path.join(OUT_FOLDER,str(NEURON_ID)+'_mesh.stl')
 DATA_C = '/n/coxfs01/leek/results/ECS_iarpa_20u_cube/segmentation.h5'
 DATA_J = '/home/harvard/2017/data/bf/1017/synapse.h5'
 MAX_COUNT = 10
 
 VOL = {
     'maxz': False,
-    'given': DATA_J,
-    'id': NEURON_ID
+    'given': DATA_J
 }
-id_set = countlabels(**VOL)
+id_set = countlabels(**VOL) - set([0])
 max_id = min(MAX_COUNT or len(id_set), len(id_set))
 
 for labid in sorted(id_set)[:max_id]:
-    VOL['id'] = labid
-    volume, offset = threshbound(**VOL)
+    OUTNAME = os.path.join(OUT_FOLDER,str(labid)+'_mesh.stl')
+    volume, offset = threshbound(id=labid, **VOL)
     smoothmesh(volume, OUTNAME, offset)
-    print('Saved id '+str(VOL['id'])+'\n')
+    print('Saved id '+str(labid)+'\n')
