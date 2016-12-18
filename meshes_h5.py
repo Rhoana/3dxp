5b1ca492130049013ff7f2567c21d9b17c3f2d0d
thejohnhoffer <hoffexi@gmail.com>
2016-12-17 23:29:26 -0500

    add scripts for multiple id values

---
+++ meshes_h5.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from toMesh import smoothmesh
+from toMesh import threshbound
+
+OUT_FOLDER = sys.argv[2]
+NEURON_ID = int(sys.argv[1])
+OUTNAME = os.path.join(OUT_FOLDER,str(NEURON_ID)+'_mesh.stl')
+DATA_C = '/n/coxfs01/leek/results/ECS_iarpa_20u_cube/segmentation.h5'
+DATA_J = '/home/harvard/2017/data/bf/1017/synapse.h5'
+
+VOL = {
+    'maxz': False,
+    'given': DATA_J,
+    'id': NEURON_ID
+}
+
+volume, offset = threshbound(**VOL)
+smoothmesh(volume, OUTNAME, offset)
+print('Saved id '+str(FIXED['id'])+'\n')
