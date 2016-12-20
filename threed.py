b364f9d308f358f0343d59cb79cc7b71ea731d98
Daniel Haehn <haehn@seas.harvard.edu>
2016-12-20 15:12:31 -0500

    Restructure the code. Much shorter and better now.

--- threed.py
+++ threed.py
@@ -1,9 +1,14 @@
+import glob
+import h5py
 import mahotas as mh
 import math
-from stl import mesh
 import numpy as np
+import os
+from stl import mesh
 import scipy
 from skimage import measure
+from xml.etree import ElementTree
+
 
 class ThreeD:
 
@@ -51,7 +56,7 @@ class ThreeD:
     return out
 
   @staticmethod
-  def create_mesh(vol, filename):
+  def create_stl(vol, filename, X=0, Y=0, tilewidth=1024):
 
     verts, faces = measure.marching_cubes(vol, 0, gradient_direction='ascent')
     applied_verts = verts[faces]
@@ -59,10 +64,214 @@ class ThreeD:
 
     mesh_data = np.zeros(vert_count, dtype=mesh.Mesh.dtype)
 
+    # Z, Y and X offsets
+    offset = np.array([0, Y*tilewidth, X*tilewidth])
+
     for i, v in enumerate(applied_verts):
-        mesh_data[i][1] = v# + bboff
+        mesh_data[i][1] = v + offset
 
     m = mesh.Mesh(mesh_data)
-    with open(filename, 'w') as f:
-        m.save(filename, f)
+    # with open(filename, 'w') as f:
+    m.save(filename)
+
     return m    
+
+
+  @staticmethod
+  def run(datafile, X, Y, outdir, tilewidth=1024):
+
+    # create output folder
+    if not os.path.exists(outdir):
+      os.makedirs(outdir)
+
+    with h5py.File(datafile, 'r') as f:
+      vol = f[f.keys()[0]][0:20,Y*tilewidth:Y*tilewidth+tilewidth,X*tilewidth:X*tilewidth+tilewidth]
+
+    # grab all IDs
+    all_ids = np.unique(vol)
+
+    print 'Loaded data..'
+
+    for id in all_ids:
+
+      # skip 0
+      outfile = str(id) + '_' + str(X) + '_' + str(Y) + '.stl'
+      if id == 0 or os.path.exists(os.path.join(outdir, outfile)):
+        continue
+
+      try:
+
+        # 1. thresholding
+        thresholded = ThreeD.threshold(vol, id)
+        for z in range(thresholded.shape[0]):
+            thresholded[z] = mh.close_holes(thresholded[z])
+
+        thresholded = np.repeat(thresholded, 10, axis=0)
+
+        # 2. smoothing along Y
+        thresholded_swapped = np.swapaxes(thresholded, 0, 1)
+        thresholded_padded = np.pad(thresholded_swapped, 2, mode='constant')
+        smoothed = ThreeD.smoothen(thresholded_padded)
+
+        # 3. marching cubes
+        smoothed = np.swapaxes(smoothed, 0, 1) # Z,Y,X
+        outfile = str(id) + '_' + str(X) + '_' + str(Y) + '.stl'
+        ThreeD.create_stl(smoothed, os.path.join(outdir, outfile), X=X, Y=Y, tilewidth=tilewidth)
+
+        print 'Stored', outfile
+
+      except:
+
+        print 'Skipped', id
+        continue
+
+    print 'All done.'
+
+  @staticmethod
+  def create_website(stldir, outputfolder, ids=None):
+
+    stl_files = None
+
+    if ids == None:
+      # grab all ids
+      stl_files = sorted([os.path.basename(v) for v in glob.glob(os.path.join(stldir, '*.stl'))])
+
+    else:
+
+      # use the specified list
+      stl_files = []
+      stl_files = [os.path.basename(v) for id in ids for v in glob.glob(os.path.join(stldir, str(id)+'_*.stl'))]
+
+
+    html_header = '''
+<html>
+<head>
+  <script type='text/javascript' src='https://www.x3dom.org/x3dom/release/x3dom.js'></script>
+  <script>
+
+  window.onload = function() {
+
+    document.getElementById('r').runtime.showAll();
+
+  };
+
+  </script>
+</head>
+<body style='padding:0px; margin:0px; overflow:hidden; background-color: #000'>
+  <x3d id='r' width='100%' height='100%'>
+
+
+
+    <scene>
+      <viewpoint position='0 0 10' ></viewpoint>
+    '''
+
+    html_content = ''
+
+    html_footer = '''
+    </scene>
+  </x3d>
+</body>
+</html>
+    '''
+
+    
+    for f in stl_files:
+
+      ID = int(f.split('_')[0])
+
+      stl_file = os.path.join(stldir, f)
+      x3d_file = os.path.join(outputfolder, f.replace('.stl', '.x3d'))
+      mipmaps = f.replace('.stl', '_')
+      html_file = os.path.join(outputfolder, f.replace('.stl', '.html'))
+
+      x3d_cmd = 'aopt -i '+ stl_file +' -x '+ x3d_file
+      r = os.system(x3d_cmd)
+      mipmap_cmd = 'aopt -i '+ x3d_file + ' -K ' + mipmaps + ':pb -N '+ html_file
+      r2 = os.system('cd ' + outputfolder + ' && ' + mipmap_cmd)
+
+      if r == 0 and r2 == 0:
+        print 'Generated X3D for', f
+      else:
+        print 'Error for', f
+        continue
+
+      # grab popGeometry HTML
+      e = ElementTree.parse(os.path.join(outputfolder, f.replace('.stl', '.html'))).getroot()
+      geometrynode = e.find('.//popGeometry')
+      geometrytext = ElementTree.tostring(geometrynode)
+      geometrytext = geometrytext.replace('primType="&quot;TRIANGLES&quot;"', "primType='\"TRIANGLES\"'")
+      geometrytext = geometrytext.replace(' />', '></popGeometryLevel>')
+
+      # create html
+      color = np.mod(107*ID,700), np.mod(509*ID,700), np.mod(200*ID,700)
+
+      mesh_html = '''
+      <shape>
+        <appearance>
+          <material diffuseColor='$COLOR'></material>
+        </appearance>
+      '''.replace('$COLOR', str(color[0]/255.) + ' ' + str(color[1]/255.) + ' ' + str(color[2]/255.))
+
+      mesh_html += '\n          ' + geometrytext
+      mesh_html += '\n      ' + '</shape>'
+
+      html_content += mesh_html + '\n'
+
+      print 'Generated HTML for', f
+
+    # return html_content
+
+    with open(os.path.join(outputfolder, 'index.html'), 'w') as f:
+      f.write(html_header + html_content + html_footer)
+
+    print 'Stored index.html.'      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
