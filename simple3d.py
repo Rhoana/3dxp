55ea934628cf10428b877e46445f97c016748d68
John Hoffer <hoffexi@gmail.com>
2017-01-06 12:43:20 -0500

    add custom html output filename

--- simple3d.py
+++ simple3d.py
@@ -7,13 +7,14 @@ DATA = HOME + '/2017/data/bf/jan_push/8x_downsampled_segmentation.h5'
 ROOTDIR = HOME + '/2017/data/3dxp/jan_push/'
 STLFOLDER = ROOTDIR + 'stl'
 X3DFOLDER = ROOTDIR + 'x3d'
-ALL_IDS = [1,2,3,4]
+ALL_IDS = [1,2,3]
+INDEX = '123.html'
 
 
 subvols = zip(*np.where(np.ones([2]*3)))
 
-for z,y,x in subvols:
-    ThreeD.run(DATA, z, y, x, STLFOLDER, 200, ALL_IDS)
+#for z,y,x in subvols:
+#    ThreeD.run(DATA, z, y, x, STLFOLDER, 200, ALL_IDS)
 
-ThreeD.create_website(STLFOLDER, X3DFOLDER, [1,4])
+ThreeD.create_website(STLFOLDER, X3DFOLDER, [1], INDEX)
 
