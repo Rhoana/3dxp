8b2224a409214c6331a8742572fac10f5b4a8b3a
John Hoffer <hoffexi@gmail.com>
2017-01-28 12:33:46 -0500

    project specific python script

--- simple3d.py
+++ simple3d.py
@@ -3,38 +3,19 @@ import argparse
 import numpy as np
 from threed import ThreeD
 
-if __name__ == "__main__":
+def main(_hd5_name, _out_name, _n_ids, _tile_size, _index):
 
-    help = {
-        'hd5': 'input hd5 file (default hd5)',
-        'out': 'output web directory (default www)',
-        'o': 'output filename (default index.html)',
-        's': 'load h5 in s*s*s chunks (default 256)',
-        'n': 'make meshes for the top n ids (default 1)',
-        'help': 'Make an hdf5 file into html meshes!'
-    }
-
-    parser = argparse.ArgumentParser(description=help['help'])
-    parser.add_argument('hd5', default='hd5', nargs='?', help=help['hd5'])
-    parser.add_argument('out', default='mojo', nargs='?', help=help['out'])
-    parser.add_argument('-s', type=int, default=256, help=help['s'])
-    parser.add_argument('-n', type=int, default=1, help=help['n'])
-    parser.add_argument('-o', default='index.html', help=help['o'])
-
-    # attain all arguments
-    args = vars(parser.parse_args())
     # expand all system paths
-    realpath = lambda pathy: os.path.realpath(os.path.expanduser(args[pathy]))
+    realpath = lambda pathy: os.path.realpath(os.path.expanduser(pathy))
 
-
-    N_TOP_IDS = args['n'] + 1
-    ROOTDIR = realpath('out')
-    DATA = realpath('hd5')
+    N_TOP_IDS = _n_ids + 1
+    ROOTDIR = realpath(_out_name)
+    DATA = realpath(_hd5_name)
     STLFOLDER = os.path.join(ROOTDIR ,'stl')
     X3DFOLDER = os.path.join(ROOTDIR , 'x3d')
     ALL_IDS = np.loadtxt(os.path.join(ROOTDIR,'out.txt'),dtype=np.uint32)[-N_TOP_IDS:-1]
-    TILESIZE = args['s']
-    INDEX = args['o']
+    TILESIZE = _tile_size
+    INDEX = _index
     SIZES = []
 
     with h5py.File(DATA, 'r') as df:
@@ -55,3 +36,25 @@ if __name__ == "__main__":
 
     DIMZ,DIMY,DIMX = SIZES
     ThreeD.create_website(STLFOLDER, X3DFOLDER, ALL_IDS, INDEX, DIMX, DIMY, DIMZ)
+
+if __name__ == "__main__":
+
+    help = {
+        'hd5': 'input hd5 file (default hd5)',
+        'out': 'output web directory (default www)',
+        'o': 'output filename (default index.html)',
+        's': 'load h5 in s*s*s chunks (default 256)',
+        'n': 'make meshes for the top n ids (default 1)',
+        'help': 'Make an hdf5 file into html meshes!'
+    }
+
+    parser = argparse.ArgumentParser(description=help['help'])
+    parser.add_argument('hd5', default='hd5', nargs='?', help=help['hd5'])
+    parser.add_argument('out', default='mojo', nargs='?', help=help['out'])
+    parser.add_argument('-s', type=int, default=256, help=help['s'])
+    parser.add_argument('-n', type=int, default=1, help=help['n'])
+    parser.add_argument('-o', default='index.html', help=help['o'])
+
+    # attain all arguments
+    args = vars(parser.parse_args())
+    main(args['hd5'],args['out'],args['n'],args['s'],args['o'])
