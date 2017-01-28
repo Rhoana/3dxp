576e93d8770d5b57dcbb68a1a37a9b1bb169ad2b
John Hoffer <hoffexi@gmail.com>
2017-01-28 11:58:47 -0500

    make simple3d.py into a script

--- simple3d.py
+++ simple3d.py
@@ -1,33 +1,57 @@
 import os, h5py
+import argparse
 import numpy as np
 from threed import ThreeD
 
-N_TOP_IDS = 4 +1
-HOME = os.path.expanduser('~')
-DATA = HOME + '/2017/data/seg_100x4x4/stitched_seg.h5'
-ROOTDIR = HOME + '/2017/winter/3dxp1338/X3DOM/seg_100x4x4/'
-STLFOLDER = ROOTDIR + 'stl'
-X3DFOLDER = ROOTDIR + 'x3d'
-ALL_IDS = np.loadtxt(os.path.join(ROOTDIR,'out.txt'),dtype=np.uint32)[-N_TOP_IDS:-1]
-INDEX = 'one.html'
-TILESIZE = 256
-SIZES = []
+if __name__ == "__main__":
 
-with h5py.File(DATA, 'r') as df:
-    SIZES = df[df.keys()[0]].shape
-    ntiles = np.array(SIZES)//TILESIZE
-    z_base = 1./ntiles[0]
-    y_base = z_base/ntiles[1]
-    x_base = y_base/ntiles[2]
+    help = {
+        'hd5': 'input hd5 file (default hd5)',
+        'out': 'output web directory (default www)',
+        'o': 'output filename (default index.html)',
+        's': 'load h5 in s*s*s chunks (default 256)',
+        'n': 'make meshes for the top n ids (default 1)',
+        'help': 'Make an hdf5 file into html meshes!'
+    }
 
-subvols = zip(*np.where(np.ones(ntiles)))
+    parser = argparse.ArgumentParser(description=help['help'])
+    parser.add_argument('hd5', default='hd5', nargs='?', help=help['hd5'])
+    parser.add_argument('out', default='mojo', nargs='?', help=help['out'])
+    parser.add_argument('-s', type=int, default=256, help=help['s'])
+    parser.add_argument('-n', type=int, default=1, help=help['n'])
+    parser.add_argument('-o', default='index.html', help=help['o'])
 
-for z,y,x in subvols:
-    #ThreeD.run(DATA, z, y, x, STLFOLDER, TILESIZE, ALL_IDS)
-
-    z_done = z*z_base + y*y_base + x*x_base
-    print("%.1f%% done with stl" % (100*z_done) )
+    # attain all arguments
+    args = vars(parser.parse_args())
+    # expand all system paths
+    realpath = lambda pathy: os.path.realpath(os.path.expanduser(args[pathy]))
 
 
-DIMZ,DIMY,DIMX = SIZES
-ThreeD.create_website(STLFOLDER, X3DFOLDER, ALL_IDS, INDEX, DIMX, DIMY, DIMZ)
+    N_TOP_IDS = args['n'] + 1
+    ROOTDIR = realpath('out')
+    DATA = realpath('hdf5')
+    STLFOLDER = ROOTDIR + 'stl'
+    X3DFOLDER = ROOTDIR + 'x3d'
+    ALL_IDS = np.loadtxt(os.path.join(ROOTDIR,'out.txt'),dtype=np.uint32)[-N_TOP_IDS:-1]
+    TILESIZE = args['s']
+    INDEX = args['o']
+    SIZES = []
+
+    with h5py.File(DATA, 'r') as df:
+        SIZES = df[df.keys()[0]].shape
+        ntiles = np.array(SIZES)//TILESIZE
+        z_base = 1./ntiles[0]
+        y_base = z_base/ntiles[1]
+        x_base = y_base/ntiles[2]
+
+    subvols = zip(*np.where(np.ones(ntiles)))
+
+    for z,y,x in subvols:
+        #ThreeD.run(DATA, z, y, x, STLFOLDER, TILESIZE, ALL_IDS)
+
+        z_done = z*z_base + y*y_base + x*x_base
+        print("%.1f%% done with stl" % (100*z_done) )
+
+
+    DIMZ,DIMY,DIMX = SIZES
+    ThreeD.create_website(STLFOLDER, X3DFOLDER, ALL_IDS, INDEX, DIMX, DIMY, DIMZ)
