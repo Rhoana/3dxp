5ce582f1b85821b281d1527a82064f346c752162
John Hoffer <hoffexi@gmail.com>
2017-01-30 11:14:26 -0500

    fix h5 loading in deepest.py

--- getmesh/scripts/deepest.py
+++ getmesh/scripts/deepest.py
@@ -21,8 +21,7 @@ def start(_argv):
 
     with h5py.File(DATA, 'r') as df:
         vol = df[df.keys()[0]]
-        sizes = vol.shape
-        ntiles = np.array(sizes)//TILESIZE
+        ntiles = np.array(vol.shape)//TILESIZE
 
         subvols = zip(*np.where(np.ones(ntiles[1:])))
         subdepths = np.arange(ntiles[0])
@@ -30,36 +29,36 @@ def start(_argv):
         y_base = z_base/ntiles[1]
         x_base = y_base/ntiles[2]
 
-    def even_count(old_c, new_c):
-        diff_count = len(new_c) - len(old_c)
-        if diff_count > 0:
-            old_c = np.r_[old_c, np.zeros(diff_count, dtype=old_c.dtype)]
-        if diff_count < 0:
-            new_c = np.r_[new_c, np.zeros(-diff_count, dtype=new_c.dtype)]
-        return old_c, new_c
+        def even_count(old_c, new_c):
+            diff_count = len(new_c) - len(old_c)
+            if diff_count > 0:
+                old_c = np.r_[old_c, np.zeros(diff_count, dtype=old_c.dtype)]
+            if diff_count < 0:
+                new_c = np.r_[new_c, np.zeros(-diff_count, dtype=new_c.dtype)]
+            return old_c, new_c
 
-    for z in subdepths:
-        z_count = np.array([],dtype=np.bool)
-        for y,x in subvols:
+        for z in subdepths:
+            z_count = np.array([],dtype=np.bool)
+            for y,x in subvols:
 
-            zo,ze = np.array([z,z+1])*TILESIZE
-            yo,ye = np.array([y,y+1])*TILESIZE
-            xo,xe = np.array([x,x+1])*TILESIZE
-            in_block = np.unique(vol[zo:ze, yo:ye, xo:xe])
-            new_count = np.zeros(max(in_block)+1, dtype=np.bool)
-            new_count[in_block] = True
+                zo,ze = np.array([z,z+1])*TILESIZE
+                yo,ye = np.array([y,y+1])*TILESIZE
+                xo,xe = np.array([x,x+1])*TILESIZE
+                in_block = np.unique(vol[zo:ze, yo:ye, xo:xe])
+                new_count = np.zeros(max(in_block)+1, dtype=np.bool)
+                new_count[in_block] = True
 
-            z_count, new_count = even_count(z_count, new_count)
-            z_count = np.logical_or(z_count, new_count)
+                z_count, new_count = even_count(z_count, new_count)
+                z_count = np.logical_or(z_count, new_count)
 
-            z_done = z*z_base + y*y_base + x*x_base
-            print("%.1f%% done with counting" % (100*z_done) )
+                z_done = z*z_base + y*y_base + x*x_base
+                print("%.1f%% done with counting" % (100*z_done) )
 
-        COUNTS, z_count = even_count(COUNTS, z_count)
-        COUNTS = COUNTS + z_count.astype(np.uint32)
+            COUNTS, z_count = even_count(COUNTS, z_count)
+            COUNTS = COUNTS + z_count.astype(np.uint32)
 
-    np.savetxt(COUNTPATH, COUNTS, fmt='%i')
-    return sort(COUNTS)
+        np.savetxt(COUNTPATH, COUNTS, fmt='%i')
+        return sort(COUNTS)
 
 def parseArgv(argv):
     sys.argv = argv
