29a6951f68697fa3e2d0397e2803c35daf304cde
John Hoffer <thejohnhoffer@rclogin07.rc.fas.harvard.edu>
2017-06-16 15:11:59 -0400

    more efficient function to update stl structure

--- PYTHON/scripts/move_folders.py
+++ PYTHON/scripts/move_folders.py
@@ -25,42 +25,41 @@ if __name__ == '__main__':
     STEP = args['step']
 
     SOURCE="/n/coxfs01/thejohnhoffer/R0/ids-2017-05-11_ids-0z-3xy_mesh-6xyz/meshes/stl/"
-    MAX_ID = 7179095
-    N_BLOCKS = 8
 
     TARGET="/n/coxfs01/thejohnhoffer/R0/ids-2017-05-11_ids-0z-3xy_mesh-8xyz/meshes/stl/" 
 
-    # Get grid of all smaller stl files
-    blocksize = (N_BLOCKS, N_BLOCKS, N_BLOCKS)
-    blocklist = range(np.prod(blocksize))
-    all_blocks =  zip(*np.unravel_index(blocklist, blocksize))
+    # Get all stl files in directory
+    old_stl = os.listdir(SOURCE)
+    n_stl = len(old_stl)
 
     # Find all IDs to make for this trial
-    step_ids = np.linspace(0, MAX_ID+1, RUNS + 1, dtype=np.uint64)
+    step_ids = np.linspace(0, n_stl, RUNS + 1, dtype=np.uint64)
     start, stop = np.uint64(step_ids[STEP:][:2])
     msg = """
-    Moving all stl files for ids from {} to {}
+    Moving all stl files for all files from {} to {}
     """.format(start, stop)
     print(msg)
 
     # Move each id
-    for i in range(start,stop):
+    for old_name in old_stl[start:stop]:
+
+        # Get the full old file path
+        old_path = os.path.join(SOURCE, old_name)
+        # Get the neuron from the file name
+        old_parts = old_name.split('_')
+        neuron = old_parts[0]
+
         # Make the new folder if doesn't exist
-        new_folder = os.path.join(TARGET, str(i))
+        new_folder = os.path.join(TARGET, neuron)
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
-        # For all the blocks
-        for bz,by,bx in all_blocks:
-            # Move to a new folder
-            old_name = '{}_{}_{}_{}.stl'.format(i,bz,by,bx)
-            old_path = os.path.join(SOURCE, old_name)
-            # If the old path exists
-            if os.path.exists(old_path):
-                # Rename the old file with the new name
-                new_name = '{}_{}_{}.stl'.format(bz,by,bx)
-                new_path = os.path.join(new_folder, new_name)
-                # Declare the renaming of this file
-                msg = "Moving {} to {}".format(old_path, new_path)
-                print(msg)
-                # Actually move the files
-                os.rename(old_path, new_path)
+
+        # Rename the old file with the new name
+        new_name = '_'.join(old_parts[1:])
+        new_path = os.path.join(new_folder, new_name)
+
+        # Declare the renaming of this file
+        msg = "Moving {} to {}".format(old_path, new_path)
+        print(msg)
+        # Actually move the files
+        os.rename(old_path, new_path)
