65ba7abb0801a2e65cb3c20e583fade832534133
John Hoffer <thejohnhoffer@coxgpu04.rc.fas.harvard.edu>
2017-09-14 11:51:17 -0400

    subclass toMojo scripts

---
+++ PYTHON/conversion/toMojo/np2mojo.py
@@ -0,0 +1,123 @@
+import os
+import math
+import lxml
+import numpy as np
+
+
+class MojoSave:
+    def __init__(self, mojo_dir):
+
+        self.tile_y = 512
+        self.tile_x = 512
+
+
+    def load_tile(self, tile, new_width, new_height, stride):
+        """ Subclass must implement
+        """
+        pass
+    
+    def save_tile(self, tile_name, tile, index_x, index_y):
+        """ Subclass must implement
+        """
+        msg = """Saving {}
+        """.format(file_path)
+        print(msg)
+
+    def save_xml(self, in_shape, dxgi='R8_UNorm', nbytes=1):
+        """ Subclass may overwrite
+        """
+        # Get the full width, depth, height
+        all_shape = self.round(in_shape)
+        (depth, width, height) = all_shape
+        #Output TiledVolumeDescription xml file
+        tiledVolumeDescription = lxml.etree.Element( "tiledVolumeDescription",
+            fileExtension = self.output_extension[1:],
+            numTilesX = str( int( math.ceil( width / self.tile_x ) ) ),
+            numTilesY = str( int( math.ceil( height / self.tile_y ) ) ),
+            numTilesZ = str( depth ),
+            numTilesW = str( self.tile_index_w ),
+            numVoxelsPerTileX = str( self.tile_x ),
+            numVoxelsPerTileY = str( self.tile_y ),
+            numVoxelsPerTileZ = str( 1 ),
+            numVoxelsX = str( width ),
+            numVoxelsY = str( height ),
+            numVoxelsZ = str( depth ),
+            dxgiFormat = str( dxgi ),
+            numBytesPerVoxel = str( nbytes ),
+            isSigned = str( False ).lower() )
+
+        with open( self.output_tile_volume_file, 'w' ) as file:
+            file.write( lxml.etree.tostring( tiledVolumeDescription, pretty_print = True ) )
+
+    def count_ids(self, image):
+        """ Subclass may implement
+        """
+        pass
+
+    def save_db(self):
+        """ Subclass may implement
+        """
+        pass
+    
+    def mkdir_safe( self, dir_to_make ):
+        if not os.path.exists( dir_to_make ):
+            os.makedirs(dir_to_make)
+
+    def round(self,shape):
+        logs = np.ceil(np.log2(shape)).astype(np.uint32)
+        padshape = tuple(2 ** p for p in logs)
+        if len(shape) > 2:
+            return (shape[0],)+padshape[1:]
+        return padshape
+
+    def run(self,input_image,tile_index_z):
+        """ Save file for each z section
+        """
+        in_shape = input_image.shape
+        pad_shape = self.round(in_shape)
+        original_image = np.zeros(pad_shape,dtype = input_image.dtype)
+        original_image[:in_shape[0],:in_shape[1]] = input_image
+
+        (original_x, original_y) = original_image.shape
+
+        # Count IDs if defined
+        self.count_ids(original_image)
+
+        current_image_y = original_y
+        current_image_x = original_x
+        self.tile_index_z = tile_index_z
+        self.tile_index_w = 0
+        stride = 1
+
+        while current_image_y > self.tile_y / 2 or current_image_x > self.tile_x / 2:
+
+            current_tile_path    = self.output_tile_path     + os.sep + 'w=' + '%08d' % ( self.tile_index_w ) + os.sep + 'z=' + '%08d' % ( self.tile_index_z )
+
+            self.mkdir_safe( current_tile_path )
+
+            current_image = self.load_tile(original_image, current_image_x, current_image_y, stride)
+
+            num_tiles_y = int( math.ceil( float( current_image_y ) / self.tile_y ) )
+            num_tiles_x = int( math.ceil( float( current_image_x ) / self.tile_x ) )
+
+            for tile_index_y in range( num_tiles_y ):
+                for tile_index_x in range( num_tiles_x ):
+
+                    y = tile_index_y * self.tile_y
+                    x = tile_index_x * self.tile_x
+
+                    current_tile_name = current_tile_path + os.sep + 'y=' + '%08d' % ( tile_index_y ) + ','  + 'x=' + '%08d' % ( tile_index_x ) + self.output_extension
+
+                    tile_image = current_image[y:y + self.tile_y, x:x + self.tile_x]
+                    self.save_tile(current_tile_name, tile_image, tile_index_x, tile_index_y)
+
+            current_image_y /= 2
+            current_image_x /= 2
+            self.tile_index_w += 1
+            stride *= 2
+
+    def save(self, in_shape):
+        """ Both functions called can be overwritten
+        """
+        self.save_db()
+        self.save_xml(in_shape)
--- PYTHON/conversion/toMojo/np2seg.py
+++ PYTHON/conversion/toMojo/np2seg.py
@@ -1,113 +1,79 @@
 import os
-import math
-import h5py
-import lxml
-import sqlite3
-import lxml.etree
 import numpy as np
+from np2mojo import MojoSave
+from .. import color_ids
+import sqlite3
+import h5py
 
-class MojoSeg:
-    def __init__(self, output_dir):
 
-        self.tile_num_pixels_y             = 512
-        self.tile_num_pixels_x             = 512
+class MojoSeg(MojoSave):
+    def __init__(self, mojo_dir):
 
-        output_path                   = output_dir
-        output_ids_path                = output_path + os.sep + 'ids'
-        ncolors                       = 1000
-        
-        
-        self.output_tile_ids_path      = output_ids_path + os.sep + 'tiles'
-        self.output_tile_volume_file       = output_ids_path + os.sep + 'tiledVolumeDescription.xml'
-        self.output_color_map_file         = output_ids_path + os.sep + 'colorMap.hdf5'
-        self.output_segment_info_db_file   = output_ids_path + os.sep + 'segmentInfo.db'
+        super(MojoSeg, self).__init__(mojo_dir)
+
+        output_path = os.path.join(mojo_dir, 'ids')
+        self.output_tile_path = os.path.join(output_path, 'tiles')
+        self.output_tile_volume_file = os.path.join(output_path, 'tiledVolumeDescription.xml')
+
+        self.output_extension     = '.hdf5'
+        ncolors = 1000
+
+        self.output_color_map_file         = output_path + os.sep + 'colorMap.hdf5'
+        self.output_segment_info_db_file   = output_path + os.sep + 'segmentInfo.db'
 
         self.id_max               = 0
         self.id_counts            = np.array([0], dtype=np.int64 )
         self.id_tile_list         = []
 
         # Make a color map
-        self.color_map = np.zeros( (ncolors + 1, 3), dtype=np.uint8 )
-        for color_i in xrange( 1, ncolors + 1 ):
-            rand_vals = np.random.rand(3)
-            self.color_map[ color_i ] = [ rand_vals[0]*255, rand_vals[1]*255, rand_vals[2]*255 ]
+        self.color_map = color_ids(np.arange(n_colors+1, dtype=np.uint64))
 
-    def round(self,shape):
-        logs = np.ceil(np.log2(shape)).astype(np.uint32)
-        padshape = tuple(2 ** p for p in logs)
-        if len(shape) > 2:
-            return (shape[0],)+padshape[1:]
-        return padshape
+    def load_tile(self, tile, new_width, new_height, stride):
+        """ Required method to load tiles
+        """
+        super(MojoSeg, self).load_tile(tile, new_width, new_height, stride)
+        return tile[ ::stride, ::stride ]
 
-    def run(self,input_ids,tile_index_z):
+    def save_tile(self, tile_name, tile, index_x, index_y):
+        """ Required method to save tiles
+        """
+        super(MojoSeg, self).save_tile(tile_name, tile, index_x, index_y)
 
-        in_shape = input_ids.shape
-        pad_shape = self.round(in_shape)
-        original_ids = np.zeros(pad_shape,dtype = input_ids.dtype)
-        original_ids[:in_shape[0],:in_shape[1]] = input_ids
+        tile_padded = np.zeros( ( self.tile_y, self.tile_x ), np.uint32 )
+        tile_padded[ 0:tile.shape[0], 0:tile.shape[1] ] = tile[:,:]
+        # Save the hdf5 file
+        with h5py.File( tile_name, 'w' ) as hf:
+            hf.create_dataset( 'IdMap', data=tile_padded )
 
-        (original_ids_num_pixels_x, original_ids_num_pixels_y) = original_ids.shape
+        position = (self.tile_index_w, self.tile_index_z, index_y, index_x)
+        for unique_id in np.unique( tile_padded ):
+            self.id_tile_list.append( (unique_id,) + position  )
 
-        ## Grow regions until there are no boundaries
+    def save_xml(self, in_shape):
+        """ Set Segmentation values for saving xml
+        """
+        super(MojoSeg, self).save_xml(in_shape, 'R32_UInt', 4)
 
-        current_ids_counts = np.bincount( original_ids.ravel() )
-        current_ids_counts_ids = np.nonzero( current_ids_counts )[0]
-        current_max = np.max( current_ids_counts_ids )
-        self.tile_index_z = tile_index_z
+    def count_ids(self, image):
+        """ Optional method to count segments
+        """
+        super(MojoSeg, self).count_ids(image)
+
+        id_counts = np.bincount( image.ravel() )
+        nonzero_ids = np.nonzero( id_counts )[0]
+        current_max = np.max( nonzero_ids )
 
         if self.id_max  < current_max:
             self.id_max  = current_max
             self.id_counts.resize( self.id_max  + 1 )
 
-        self.id_counts[ current_ids_counts_ids ] = self.id_counts[ current_ids_counts_ids ] + np.int64( current_ids_counts [ current_ids_counts_ids ] )
+        self.id_counts[ nonzero_ids ] += np.int64( id_counts [ nonzero_ids ] )
 
-        current_ids_num_pixels_y = original_ids_num_pixels_y
-        current_ids_num_pixels_x = original_ids_num_pixels_x
-        current_tile_data_space_y  = self.tile_num_pixels_y
-        current_tile_data_space_x  = self.tile_num_pixels_x
-        self.tile_index_w          = 0
-        ids_stride                 = 1
+    def save_db(self):
+        """ Optional method to make a database
+        """
+        super(MojoSeg, self).save_db()
 
-        while current_ids_num_pixels_y > self.tile_num_pixels_y / 2 or current_ids_num_pixels_x > self.tile_num_pixels_x / 2:
-
-            current_tile_ids_path    = self.output_tile_ids_path     + os.sep + 'w=' + '%08d' % ( self.tile_index_w ) + os.sep + 'z=' + '%08d' % ( self.tile_index_z )
-
-            self.mkdir_safe( current_tile_ids_path )
-
-            current_ids = original_ids[ ::ids_stride, ::ids_stride ]
-
-            num_tiles_y = int( math.ceil( float( current_ids_num_pixels_y ) / self.tile_num_pixels_y ) )
-            num_tiles_x = int( math.ceil( float( current_ids_num_pixels_x ) / self.tile_num_pixels_x ) )
-
-            for tile_index_y in range( num_tiles_y ):
-                for tile_index_x in range( num_tiles_x ):
-
-                    y = tile_index_y * self.tile_num_pixels_y
-                    x = tile_index_x * self.tile_num_pixels_x
-
-                    current_tile_ids_name    = current_tile_ids_path    + os.sep + 'y=' + '%08d' % ( tile_index_y ) + ','  + 'x=' + '%08d' % ( tile_index_x ) + '.hdf5'
-
-                    tile_ids                                                                   = np.zeros( ( self.tile_num_pixels_y, self.tile_num_pixels_x ), np.uint32 )
-                    tile_ids_non_padded                                                        = current_ids[ y : y + self.tile_num_pixels_y, x : x + self.tile_num_pixels_x ]
-                    tile_ids[ 0:tile_ids_non_padded.shape[0], 0:tile_ids_non_padded.shape[1] ] = tile_ids_non_padded[:,:]
-                    self.save_hdf5( current_tile_ids_name, 'IdMap', tile_ids )
-
-                    for unique_tile_id in np.unique( tile_ids ):
-
-                        self.id_tile_list.append( (unique_tile_id, self.tile_index_w, self.tile_index_z, tile_index_y, tile_index_x ) )
-
-            current_ids_num_pixels_y = current_ids_num_pixels_y / 2
-            current_ids_num_pixels_x = current_ids_num_pixels_x / 2
-            current_tile_data_space_y  = current_tile_data_space_y  * 2
-            current_tile_data_space_x  = current_tile_data_space_x  * 2
-            self.tile_index_w          = self.tile_index_w          + 1
-            ids_stride                 = ids_stride                 * 2
-
-    def save(self,in_shape):
-
-        all_shape = self.round(in_shape)
-        (numTilesZ, original_ids_num_pixels_x, original_ids_num_pixels_y) = all_shape
-        
         ## Sort the tile list so that the same id appears together
         self.id_tile_list = np.array( sorted( self.id_tile_list ), np.uint32 )
 
@@ -115,14 +81,13 @@ class MojoSeg:
 
         print 'Writing colorMap file (hdf5)'
 
-        hdf5             = h5py.File( self.output_color_map_file, 'w' )
+        hdf5 = h5py.File( self.output_color_map_file, 'w' )
 
         hdf5['idColorMap'] = self.color_map
 
         hdf5.close()
 
         print 'Writing segmentInfo file (sqlite)'
-
 
         if os.path.exists(self.output_segment_info_db_file):
             os.remove(self.output_segment_info_db_file)
@@ -166,43 +131,3 @@ class MojoSeg:
         con.commit()
 
         con.close()
-
-        #Output TiledVolumeDescription xml file
-
-        print 'Writing TiledVolumeDescription file'
-
-        tiledVolumeDescription = lxml.etree.Element( "tiledVolumeDescription",
-            fileExtension = "hdf5",
-            numTilesX = str( int( math.ceil( original_ids_num_pixels_x / self.tile_num_pixels_x ) ) ),
-            numTilesY = str( int( math.ceil( original_ids_num_pixels_y / self.tile_num_pixels_y ) ) ),
-            numTilesZ = str( numTilesZ ),
-            numTilesW = str( self.tile_index_w ),
-            numVoxelsPerTileX = str( self.tile_num_pixels_x ),
-            numVoxelsPerTileY = str( self.tile_num_pixels_y ),
-            numVoxelsPerTileZ = str( 1 ),
-            numVoxelsX = str( original_ids_num_pixels_x ),
-            numVoxelsY = str( original_ids_num_pixels_y ),
-            numVoxelsZ = str( numTilesZ ),
-            dxgiFormat = 'R32_UInt',
-            numBytesPerVoxel = str( 4 ),
-            isSigned = str( False ).lower() )
-
-        with open( self.output_tile_volume_file, 'w' ) as file:
-            file.write( lxml.etree.tostring( tiledVolumeDescription, pretty_print = True ) )
-
-
-    def mkdir_safe( self, dir_to_make ):
-
-        if not os.path.exists( dir_to_make ):
-            execute_string = 'mkdir -p ' + '"' + dir_to_make + '"'
-            print execute_string
-            os.system( execute_string )
-
-    def save_hdf5( self, file_path, dataset_name, array ):
-
-        hdf5             = h5py.File( file_path, 'w' )
-        hdf5.create_dataset( dataset_name, data=array )
-        hdf5.close()
-
-        print file_path
-        print
